30b895f2838e1cad2287732ed894aedd48ae7029
Andrea Quaglioni <quaglioniandrea@gmail.com>
2022-07-06 15:31:28 +0200

    fix: streamlined conditions

--- js/main.js
+++ js/main.js
@@ -7,18 +7,19 @@ for (let i = 1; i<=input; i++) {
     
     addBox.className = "box";
     containerDom.append(addBox);
-
-    if (i % 3 == 0 && i % 5 != 0) {
-        addBox.classList.add('fizz');  
-        addBox.append('Fizz') 
-    } else if (i % 5 == 0 && i % 3 != 0) {
-        addBox.classList.add('buzz')
-        addBox.append('Buzz')
-    } else if (i % 3 == 0 && i % 5 == 0) {
-        addBox.classList.add('buzzfizz')
-        addBox.append('BuzzFizz')
-    } else {
-        addBox.append(i)
+    
+    
+    if (i % 3 == 0 && i % 5 == 0) {
+            addBox.classList.add('buzzfizz')
+            addBox.append('BuzzFizz')
+        } else if (i % 3 == 0) {
+            addBox.classList.add('fizz');  
+            addBox.append('Fizz') 
+        } else if (i % 5 == 0) {
+            addBox.classList.add('buzz')
+            addBox.append('Buzz')
+        }  else {
+            addBox.append(i)
     }
 
     
