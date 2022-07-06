fbcb340490ce2002ef7b2981e94d8d7ef71ba681
Andrea Quaglioni <quaglioniandrea@gmail.com>
2022-07-06 14:08:42 +0200

    complete

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const input = parseInt(prompt('Quante box vuoi visualizzare?'))
+
+const containerDom = document.getElementById('container');
+
+for (let i = 1; i<=input; i++) {
+    const addBox = document.createElement('div')
+    
+    addBox.className = "box";
+    containerDom.append(addBox);
+
+    if (i % 3 == 0 && i % 5 != 0) {
+        addBox.classList.add('fizz');  
+        addBox.append('Fizz') 
+    } else if (i % 5 == 0 && i % 3 != 0) {
+        addBox.classList.add('buzz')
+        addBox.append('Buzz')
+    } else if (i % 3 == 0 && i % 5 == 0) {
+        addBox.classList.add('buzzfizz')
+        addBox.append('BuzzFizz')
+    } else {
+        addBox.append(i)
+    }
+
+    
+}
